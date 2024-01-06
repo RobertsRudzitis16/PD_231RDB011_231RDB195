d323a8a3d1a06932be9415136da1a7ff455ba4c2
sofijanagibina <Sofija.Nagibina@edu.rtu.lv>
2024-01-06 21:00:07 +0200

    06.01.24

--- result.py
+++ result.py
@@ -20,15 +20,35 @@ with open("movie_list.csv", "r", encoding="utf-8") as file:
 
 url = "https://www.imdb.com/"
 driver.get(url)
-time.sleep(2)
+time.sleep(3)
 find = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div/button[2]')
 find.click()
+
+list = []
 
 for name in movies:
     find = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, "suggestion-search")))
     find.send_keys(name)
     find = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.ipc-metadata-list-summary-item__t")))
     find.click()
+
+    year_element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/ul/li[1]')))
+    length_element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/ul/li[3]')))
+    rating_element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/span/div/div[2]/div[1]/span[1]')))
+    popularity_element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/span/div/div[2]/div[3]')))
+    actor_element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/section/div[2]/div/ul/li[3]/div/ul')))
+    director_element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/section/div[2]/div/ul/li[1]/div/ul/li/a')))
+    description_element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/section/p/span[2]')))
+    
+    year = year_element.text
+    length = length_element.text
+    rating = rating_element.text
+    popularity = popularity_element.text
+    actor = actor_element.text
+    director = director_element.text
+    description = description_element.text
+
+    list.append([year, length, rating, popularity, actor, director, description])
 
 wb = Workbook()
 ws = wb.active
