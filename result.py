b12ea426787bfb91333d7eb1e9e57caf58185829
sofijanagibina <Sofija.Nagibina@edu.rtu.lv>
2024-01-06 17:10:59 +0200

    06.01.24

--- result.py
+++ result.py
@@ -2,6 +2,8 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from openpyxl import Workbook, load_workbook 
 
@@ -19,16 +21,13 @@ with open("movie_list.csv", "r", encoding="utf-8") as file:
 url = "https://www.imdb.com/"
 driver.get(url)
 time.sleep(2)
+find = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div/button[2]')
+find.click()
 
 for name in movies:
-    find=driver.find_element(By.ID, "suggestion-search")
+    find = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, "suggestion-search")))
     find.send_keys(name)
-
-    find=driver.find_element(By.ID, "suggestion-search-button")
-    find.click()
-    time.sleep(2)
-
-    find=driver.find_element(By.CLASS_NAME, "ipc-metadata-list-summary-item ipc-metadata-list-summary-item--click find-result-item find-title-result")
+    find = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.ipc-metadata-list-summary-item__t")))
     find.click()
     
 input()
